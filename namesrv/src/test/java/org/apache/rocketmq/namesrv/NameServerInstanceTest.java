4432db7a4908f49b2dc029abd39e603d3b491f7b
Randy <ztelur@gmail.com>
2019-12-05 15:38:02 +0800

    demo

--- namesrv/src/test/java/org/apache/rocketmq/namesrv/NameServerInstanceTest.java
+++ namesrv/src/test/java/org/apache/rocketmq/namesrv/NameServerInstanceTest.java
@@ -16,6 +16,7 @@
  */
 package org.apache.rocketmq.namesrv;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.apache.rocketmq.common.namesrv.NamesrvConfig;
 import org.apache.rocketmq.remoting.netty.NettyServerConfig;
 import org.junit.After;
@@ -35,6 +36,23 @@ public class NameServerInstanceTest {
         boolean initResult = nameSrvController.initialize();
         assertThat(initResult).isTrue();
         nameSrvController.start();
+    }
+
+
+    // NameServerInstanceTest.java
+
+    public static void main(String[] args) throws Exception {
+        // NamesrvConfig 配置
+        final NamesrvConfig namesrvConfig = new NamesrvConfig();
+        // NettyServerConfig 配置
+        final NettyServerConfig nettyServerConfig = new NettyServerConfig();
+        nettyServerConfig.setListenPort(9876); // 设置端口
+        // 创建 NamesrvController 对象，并启动
+        NamesrvController namesrvController = new NamesrvController(namesrvConfig, nettyServerConfig);
+        namesrvController.initialize();
+        namesrvController.start();
+        // 睡觉，就不起来
+        Thread.sleep(DateUtils.MILLIS_PER_DAY);
     }
 
     @After
